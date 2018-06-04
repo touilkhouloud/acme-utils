1efa5bed2ca3ca9ab24f4e54a78a67279a4aba6e
Patrick Titiano <ptitiano@baylibre.com>
2018-06-04 15:21:39 +0200

    ping: fix missing header
    Fix missing header.
    
    Signed-off-by: Patrick Titiano <ptitiano@baylibre.com>

--- pyacmecapture/ping.py
+++ pyacmecapture/ping.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
-"""
-    TBD
+""" Python Network Ping Utility
+
+Python function implementing network ping functionality.
+
+Source: https://stackoverflow.com/questions/2953462/pinging-servers-in-python
 """
 
 
 from platform import system as system_name # Returns the system/OS name
 from os import system as system_call       # Execute a shell command
 
-__app_name__ = ""
-__license__ = ""
-__copyright__ = ""
-__date__ = ""
-__author__ = ""
-__email__ =  ""
-__contact__ = ""
-__maintainer__ = ""
-__status__ = ""
-__version__ = "0.0.1"
+__app_name__ = "Ping"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018, Baylibre SAS"
+__date__ = "2018/03/01"
+__author__ = "Patrick Titiano"
+__email__ = "ptitiano@baylibre.com"
+__contact__ = "ptitiano@baylibre.com"
+__maintainer__ = "Patrick Titiano"
+__status__ = "Development"
+__version__ = "0.1"
 __deprecated__ = False
 
 
@@ -27,7 +30,6 @@ def ping(host):
     False otherwise.
     Remember that some hosts may not respond to a ping request even if the host
     name is valid.
-    Source: https://stackoverflow.com/questions/2953462/pinging-servers-in-python
 
     Args:
         host: hostname (e.g. 192.168.1.2 or myhost.mydomain)
